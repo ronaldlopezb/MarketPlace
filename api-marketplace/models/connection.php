ca3c61b94e06521c7dc334b9b2ae75f33757a805
Ronald López B <ronaldlopez@ronaldlopezb.com>
2021-04-04 13:49:45 -0500

    API: Clase 45: Conexión a la BD
    Conexión completa

---
+++ api-marketplace/models/connection.php
@@ -0,0 +1,20 @@
+<?php
+
+class Connection{
+    
+    static public function connect(){
+
+        try{
+
+            $link = new PDO("mysql:host=localhost;dbname=marketplace","root","123456");
+            $link->exec("set names utf8");
+
+        }catch(PDOException $e){
+
+            die("Error: ".$e.getMessage());
+
+        }
+        
+        return $link;   
+    }
+}
