03be6227d64197b808977773aab8fe4d012f3303
Ronald López B <ronaldlopez@ronaldlopezb.com>
2021-04-04 13:22:14 -0500

    API: Clase 43: CORS
    Control de Acceso HTTP agregado

--- api-marketplace/index.php
+++ api-marketplace/index.php
@@ -1,5 +1,11 @@
 <?php
+//// CORS : /////
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+header('content-type: application/json; charset=utf-8');
 
 require_once "controllers/route.controller.php";
+
 $index = new RoutesController(); //Instancio todo RoutesController
 $index -> index(); //Ejecuto el index()
